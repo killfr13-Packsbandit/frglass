2e23a5ad121e9fdb456c71ae4cf8eb3736b95678
killfr13-Packsbandit <killfr13@gmail.com>
2026-09-11 13:01:13 +0200

    Keep admin and API routes out of search indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/admin/", "/api/"],
     },
     sitemap: "https://frglass.at/sitemap.xml",
     host: "https://frglass.at",
