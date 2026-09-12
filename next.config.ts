5e27ac9f6ff5e005fa061b25fb4c27c41730d316
killfr13-Packsbandit <killfr13@gmail.com>
2026-09-12 23:16:50 +0200

    Redirect www to canonical frglass.at domain

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.frglass.at",
+          },
+        ],
+        destination: "https://frglass.at/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
