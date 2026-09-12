ad91a309da4ee6bd80850980945cd8928c1b1682
killfr13-Packsbandit <killfr13@gmail.com>
2026-09-12 21:00:51 +0200

    Use live R2 status check in admin session

--- src/app/api/admin/session/route.ts
+++ src/app/api/admin/session/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { isAdmin, isAdminConfigured } from "../../../../lib/adminAuth";
-import { isR2Configured } from "../../../../lib/r2Storage";
+import { checkR2Configured } from "../../../../lib/r2Storage";
 
 export const dynamic = "force-dynamic";
 
@@ -8,6 +8,6 @@ export async function GET() {
   return NextResponse.json({
     authenticated: await isAdmin(),
     authConfigured: isAdminConfigured(),
-    storageConfigured: await isR2Configured(),
+    storageConfigured: await checkR2Configured(),
   });
 }
