93a16dbcbb90421dd80d282b253f1033a7bea595
killfr13-Packsbandit <killfr13@gmail.com>
2026-09-16 12:02:08 +0200

    Add flexible text placements to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,19 @@ import Hero from "./components/Hero";
 import JewelryShowcase from "./components/Jewelry";
 import Workshop from "./components/Workshop";
 import HomeCommunityReviews from "./components/HomeCommunityReviews";
+import FlexibleTextBlocks from "./components/FlexibleTextBlocks";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white">
       <Hero />
+      <FlexibleTextBlocks page="home" placement="afterHero" />
       <JewelryShowcase />
+      <FlexibleTextBlocks page="home" placement="afterProducts" />
       <Workshop />
+      <FlexibleTextBlocks page="home" placement="afterWorkshop" />
       <HomeCommunityReviews />
+      <FlexibleTextBlocks page="home" placement="bottom" />
     </main>
   );
 }
